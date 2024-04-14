bc63269b05eda02feab6fde5244c24c5c283128f
天才小熊猫 <2938052061@qq.com>
2024-04-14 08:24:27 +0800

    完成权限功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,8 @@ import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 import Welcome from '@/components/Welcome.vue'
 import Users from '@/components/user/Users.vue'
-
+import Rights from '@/components/Power/Rights.vue'
+import Roles from '@/components/Power/Roles.vue'
 
 Vue.use(VueRouter)
 
@@ -26,6 +27,8 @@ const routes = [
     children:[
     { path: '/welcome', component: Welcome },
     { path: '/users', component: Users },
+    { path: '/rights', component: Rights },
+    { path: '/roles', component: Roles },
   ]
 },
 
