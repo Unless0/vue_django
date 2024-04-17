ec14f0649fa296780e0240be9240a1775faab6b1
天才小熊猫 <2938052061@qq.com>
2024-04-17 20:25:51 +0800

    完成商品功能的开发

--- src/main.js
+++ src/main.js
@@ -11,6 +11,15 @@ import TreeTable from 'vue-table-with-tree-grid'
 //注册为全局
 Vue.component('tree-table',TreeTable)
 
+//全局导入富文本编辑器
+import VueQuillEditor  from 'vue-quill-editor'
+//导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+//将富文本注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
 Vue.use(ElementUI);
 //配置请求的根路径
 import axios from 'axios'
@@ -21,6 +30,21 @@ axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
   //在最后必须return config
   return config
+})
+
+// 时间过滤器
+Vue.filter('dateFormat',function(originVal){
+  const dt = new Date(originVal)
+//年月日
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2,'0')
+  const d = (dt.getDate() + '').padStart(2,'0')
+//时分秒
+  const hh = (dt.getHours() + '').padStart(2,'0')
+  const mm = (dt.getMinutes() + '').padStart(2,'0')
+  const ss = (dt.getSeconds() + '').padStart(2,'0')
+  
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
 })
 
 // axios挂载到vue原型对象上
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,9 @@ import Rights from '@/components/Power/Rights.vue'
 import Roles from '@/components/Power/Roles.vue'
 import Cate from '@/components/goods/Cate.vue'
 import Params from '@/components/goods/Params.vue'
+import GoodsList from '@/components/goods/GoodsList.vue'
+import Add from '@/components/goods/Add.vue'
+
 
 Vue.use(VueRouter)
 
@@ -33,8 +36,10 @@ const routes = [
     { path: '/roles', component: Roles },
     { path: '/categories', component: Cate },
     { path: '/params', component: Params },
+    { path: '/goods', component: GoodsList },
+    { path: '/goods/add', component: Add },
   ]
-},
+}, 
 
 
 ]
